ae26236ec614dd21e1d2b6c6daa1e8b5a36d6b21
suganya05 <go2suganya@gmail.com>
2022-09-21 18:59:36 +0530

    chore: header, hero, button components

--- src/pages/Home/components/DeCharter/index.tsx
+++ src/pages/Home/components/DeCharter/index.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import "../DeCharter/DeCharter.scss";
+import Lock from "../../../../assets/logo/lock.svg";
+import Calendar from "../../../../assets/logo/calendar.svg";
+import Star from "../../../../assets/logo/star.svg";
+import Negotiation from "../../../../assets/logo/negotiation.svg";
+import { Button } from "../../../../components";
 
 const DeCharter = () => {
   return (
     <div className="deCharter-container">
       <div className="mx pad">
         <div className="deCharter">
-          <h1>njdsdhwej</h1>
+          <h1>De Charter</h1>
+          <p>
+            A highly selective professional club ⓘ for business and management
+            talent. Only those who clear our industry-backed rigourous process
+            get in. Once you are in, take advantage of a range of privileges and
+            benefits.
+          </p>
+          <div className="deCharter-content">
+            <div>
+              <img src={Lock} alt="" />
+              <h6>Priority access to</h6>
+              <h2>Job Opportunities</h2>
+            </div>
+            <div>
+              <img src={Calendar} alt="" />
+              <h6>Network & attend</h6>
+              <h2>exclusive events</h2>
+            </div>
+            <div>
+              <img src={Star} alt="" />
+              <h6>use stoa L1 / L2</h6>
+              <h2>designation</h2>
+            </div>
+            <div>
+              <img src={Negotiation} alt="" />
+              <h6>Higher salary</h6>
+              <h2>negotiating power</h2>
+            </div>
+          </div>
+          <div className="debtn">
+            <Button variant="primary" children="More about the charter" />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Home/components/DeJourney/index.tsx
+++ src/pages/Home/components/DeJourney/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "../DeJourney/DeJourney.scss";
+import EarnLevel from "../../../../assets/images/earn-level.png";
+import Learning from "../../../../assets/images/learning.png";
+import Orientation from "../../../../assets/images/Orientation.png";
+import Admission from "../../../../assets/images/admission-interview.png";
 
-const index = () => {
-  return <div>index</div>;
+const DeJourney = () => {
+  return (
+    <div className="deJourney-container">
+      <div className="mx pad">
+        <div className="deJourney">
+          <h1>
+            Your <span className="text-coloring">De Journey</span>
+          </h1>
+          <div>
+            <div>
+              <img src={EarnLevel} alt="" />
+            </div>
+            <div>
+              <h3>Admissions Interview</h3>
+              <p>
+                We check for a fit between your current profile and career goals
+                with Stoa’s offerings. Expect to be grilled on a case study,
+                guesstimate or sample business problem by our team.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default index;
+export default DeJourney;
